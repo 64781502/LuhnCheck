c204576ef7a9fefa5bc64a87c6742fb64938eb99
Andrew <60436090+TheRealOXY@users.noreply.github.com>
2022-01-09 11:49:09 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Luhn
+{
+    partial class LuhnCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LuhnCheck));
+            this.b1 = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.TextBox();
+            this.b3 = new System.Windows.Forms.TextBox();
+            this.b4 = new System.Windows.Forms.TextBox();
+            this.b5 = new System.Windows.Forms.TextBox();
+            this.b6 = new System.Windows.Forms.TextBox();
+            this.b7 = new System.Windows.Forms.TextBox();
+            this.b8 = new System.Windows.Forms.TextBox();
+            this.b9 = new System.Windows.Forms.TextBox();
+            this.b10 = new System.Windows.Forms.TextBox();
+            this.b11 = new System.Windows.Forms.TextBox();
+            this.b12 = new System.Windows.Forms.TextBox();
+            this.b13 = new System.Windows.Forms.TextBox();
+            this.b14 = new System.Windows.Forms.TextBox();
+            this.b15 = new System.Windows.Forms.TextBox();
+            this.b16 = new System.Windows.Forms.TextBox();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.CalcLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(223, 136);
+            this.b1.MaxLength = 1;
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(19, 31);
+            this.b1.TabIndex = 0;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b2.Location = new System.Drawing.Point(248, 136);
+            this.b2.MaxLength = 1;
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(19, 31);
+            this.b2.TabIndex = 1;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b3.Location = new System.Drawing.Point(273, 136);
+            this.b3.MaxLength = 1;
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(19, 31);
+            this.b3.TabIndex = 2;
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b4.Location = new System.Drawing.Point(298, 136);
+            this.b4.MaxLength = 1;
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(19, 31);
+            this.b4.TabIndex = 3;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b5.Location = new System.Drawing.Point(367, 136);
+            this.b5.MaxLength = 1;
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(19, 31);
+            this.b5.TabIndex = 4;
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b6.Location = new System.Drawing.Point(392, 136);
+            this.b6.MaxLength = 1;
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(19, 31);
+            this.b6.TabIndex = 5;
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b7.Location = new System.Drawing.Point(417, 136);
+            this.b7.MaxLength = 1;
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(19, 31);
+            this.b7.TabIndex = 6;
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b8.Location = new System.Drawing.Point(442, 136);
+            this.b8.MaxLength = 1;
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(19, 31);
+            this.b8.TabIndex = 7;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b9.Location = new System.Drawing.Point(503, 136);
+            this.b9.MaxLength = 1;
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(19, 31);
+            this.b9.TabIndex = 8;
+            // 
+            // b10
+            // 
+            this.b10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b10.Location = new System.Drawing.Point(528, 136);
+            this.b10.MaxLength = 1;
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(19, 31);
+            this.b10.TabIndex = 9;
+            // 
+            // b11
+            // 
+            this.b11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b11.Location = new System.Drawing.Point(553, 136);
+            this.b11.MaxLength = 1;
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(19, 31);
+            this.b11.TabIndex = 10;
+            // 
+            // b12
+            // 
+            this.b12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b12.Location = new System.Drawing.Point(578, 136);
+            this.b12.MaxLength = 1;
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(19, 31);
+            this.b12.TabIndex = 11;
+            // 
+            // b13
+            // 
+            this.b13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b13.Location = new System.Drawing.Point(636, 136);
+            this.b13.MaxLength = 1;
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(19, 31);
+            this.b13.TabIndex = 12;
+            // 
+            // b14
+            // 
+            this.b14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b14.Location = new System.Drawing.Point(661, 136);
+            this.b14.MaxLength = 1;
+            this.b14.Name = "b14";
+            this.b14.Size = new System.Drawing.Size(19, 31);
+            this.b14.TabIndex = 13;
+            // 
+            // b15
+            // 
+            this.b15.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b15.Location = new System.Drawing.Point(686, 136);
+            this.b15.MaxLength = 1;
+            this.b15.Name = "b15";
+            this.b15.Size = new System.Drawing.Size(19, 31);
+            this.b15.TabIndex = 14;
+            // 
+            // b16
+            // 
+            this.b16.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b16.Location = new System.Drawing.Point(711, 136);
+            this.b16.MaxLength = 1;
+            this.b16.Name = "b16";
+            this.b16.Size = new System.Drawing.Size(19, 31);
+            this.b16.TabIndex = 15;
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CalcButton.Location = new System.Drawing.Point(418, 263);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(127, 34);
+            this.CalcButton.TabIndex = 16;
+            this.CalcButton.Text = "Check";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // CalcLabel
+            // 
+            this.CalcLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CalcLabel.AutoSize = true;
+            this.CalcLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CalcLabel.Location = new System.Drawing.Point(399, 397);
+            this.CalcLabel.Name = "CalcLabel";
+            this.CalcLabel.Size = new System.Drawing.Size(0, 25);
+            this.CalcLabel.TabIndex = 17;
+            // 
+            // LuhnCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(984, 521);
+            this.Controls.Add(this.CalcLabel);
+            this.Controls.Add(this.CalcButton);
+            this.Controls.Add(this.b16);
+            this.Controls.Add(this.b15);
+            this.Controls.Add(this.b14);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LuhnCheck";
+            this.Text = "LuhnCheck";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox b1;
+        private System.Windows.Forms.TextBox b2;
+        private System.Windows.Forms.TextBox b3;
+        private System.Windows.Forms.TextBox b4;
+        private System.Windows.Forms.TextBox b5;
+        private System.Windows.Forms.TextBox b6;
+        private System.Windows.Forms.TextBox b7;
+        private System.Windows.Forms.TextBox b8;
+        private System.Windows.Forms.TextBox b9;
+        private System.Windows.Forms.TextBox b10;
+        private System.Windows.Forms.TextBox b11;
+        private System.Windows.Forms.TextBox b12;
+        private System.Windows.Forms.TextBox b13;
+        private System.Windows.Forms.TextBox b14;
+        private System.Windows.Forms.TextBox b15;
+        private System.Windows.Forms.TextBox b16;
+        private System.Windows.Forms.Button CalcButton;
+        private System.Windows.Forms.Label CalcLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Luhn
+{
+    public partial class LuhnCheck : Form
+    {
+        public LuhnCheck()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            try //dont do this kids 💀
+            {
+                int doubleB1 = Math.Abs(Convert.ToInt32(b1.Text) * 2);
+                int doubleB3 = Math.Abs(Convert.ToInt32(b3.Text) * 2);
+                int doubleB5 = Math.Abs(Convert.ToInt32(b5.Text) * 2);
+                int doubleB7 = Math.Abs(Convert.ToInt32(b7.Text) * 2);
+                int doubleB9 = Math.Abs(Convert.ToInt32(b9.Text) * 2);
+                int doubleB11 = Math.Abs(Convert.ToInt32(b11.Text) * 2);
+                int doubleB13 = Math.Abs(Convert.ToInt32(b13.Text) * 2);
+                int doubleB15 = Math.Abs(Convert.ToInt32(b15.Text) * 2);
+                int checkeddoubleB1 = 0;
+                int checkeddoubleB3 = 0;
+                int checkeddoubleB5 = 0;
+                int checkeddoubleB7 = 0;
+                int checkeddoubleB9 = 0;
+                int checkeddoubleB11 = 0;
+                int checkeddoubleB13 = 0;
+                int checkeddoubleB15 = 0;
+
+                if (doubleB1 >= 10)
+                {
+                    checkeddoubleB1 += Math.Abs(doubleB1 - 9);
+                }
+                else
+                {
+                    checkeddoubleB1 = doubleB1;
+                }
+
+
+                if (doubleB3 >= 10)
+                {
+                    checkeddoubleB3 += Math.Abs(doubleB3 - 9);
+                }
+                else
+                {
+                    checkeddoubleB3 = doubleB3;
+                }
+
+
+                if (doubleB5 >= 10)
+                {
+                    checkeddoubleB5 += Math.Abs(doubleB5 - 9);
+                }
+                else
+                {
+                    checkeddoubleB5 = doubleB5;
+                }
+
+                if (doubleB7 >= 10)
+                {
+                    checkeddoubleB7 += Math.Abs(doubleB7 - 9);
+                }
+                else
+                {
+                    checkeddoubleB7 = doubleB7;
+                }
+
+
+                if (doubleB9 >= 10)
+                {
+                    checkeddoubleB9 += Math.Abs(doubleB9 - 9);
+                }
+                else
+                {
+                    checkeddoubleB9 = doubleB9;
+                }
+
+
+                if (doubleB11 >= 10)
+                {
+                    checkeddoubleB11 += Math.Abs(doubleB11 - 9);
+                }
+                else
+                {
+                    checkeddoubleB11 = doubleB11;
+                }
+
+
+                if (doubleB13 >= 10)
+                {
+                    checkeddoubleB13 += Math.Abs(doubleB13 - 9);
+                }
+                else
+                {
+                    checkeddoubleB13 = doubleB13;
+                }
+
+
+                if (doubleB15 >= 10)
+                {
+                    checkeddoubleB15 += Math.Abs(doubleB15 - 9);
+                }
+                else
+                {
+                    checkeddoubleB15 = doubleB15;
+                }
+
+                int checkedSum = Math.Abs(checkeddoubleB1 + Convert.ToInt32(b2.Text) + checkeddoubleB3 + Convert.ToInt32(b4.Text) + checkeddoubleB5 + Convert.ToInt32(b6.Text) + checkeddoubleB7 + Convert.ToInt32(b8.Text) + checkeddoubleB9 + Convert.ToInt32(b10.Text) + checkeddoubleB11 + Convert.ToInt32(b12.Text) + checkeddoubleB13 + Convert.ToInt32(b14.Text) + checkeddoubleB15 + Convert.ToInt32(b16.Text));
+
+                if (checkedSum.ToString().EndsWith(("0")))
+                {
+                    CalcLabel.ForeColor = Color.FromArgb(0, 220, 0);
+                    CalcLabel.Text = "Credit card valid";
+                }
+                else
+                {
+                    CalcLabel.ForeColor = Color.FromArgb(255, 0, 0);
+                    CalcLabel.Text = "Credit card not valid";
+                }
+            }
+            catch (FormatException)
+            {
+                CalcLabel.ForeColor = Color.FromArgb(255, 0, 0);
+                CalcLabel.Text = "Please fill all the digits";
+            }
+        }
+
+        //ignore
+        Point lastPoint;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
